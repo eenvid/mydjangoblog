da1a42a2412bb899ae7d788170063ed86b605bb6
eenvid <eenvid@Gamil.com>
2022-09-27 09:33:56 +0330

    Session17_4 Complete

--- accounts/views.py
+++ accounts/views.py
@@ -19,7 +19,7 @@ def login_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-
+            pass
             return redirect("articles:list")
     else:
         form = AuthenticationForm()
