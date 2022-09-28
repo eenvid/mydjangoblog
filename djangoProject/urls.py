4212775a4ad309bcc2a23e523c46fd6f19134ec8
eenvid <eenvid@Gamil.com>
2022-09-29 02:04:59 +0330

    Session26 Complete

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -19,14 +19,14 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
-
+from articles.views import articlesList
 
 urlpatterns = [
-    path("", views.home),
     path("admin/", admin.site.urls),
     path("about/", views.about),
     path("articles/", include("articles.urls")),
-    path("accounts/", include("accounts.urls"))
+    path("accounts/", include("accounts.urls")),
+    path("", articlesList),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
